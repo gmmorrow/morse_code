b720dd50ac3416eae13b78e2f780dc5e2a2459da
gabrielle@Gabbys-MacPro.com <gab_morrow@yahoo.com>
2020-09-11 10:48:38 -0400

    morse maybe final

--- morse.py
+++ morse.py
@@ -9,21 +9,46 @@ Pause between dots and dashes in a character – is 1 time unit long.
 Pause between characters inside a word – is 3 time units long.
 Pause between words – is 7 time units long.
 """
-__author__ = '???'
+__author__ = 'Gabrielle'
 
 from morse_dict import MORSE_2_ASCII
+# from morse_dict import ASCII_2_MORSE
 
+
+def find_mult(bits):
+    smallest_zeros = len(min([group for group in bits.split('1') if group != '']))
+    smallest_ones = len(min([group for group in bits.split('0') if group != '']))
+    return min(smallest_zeros, smallest_ones)
 
 def decode_bits(bits):
-    # your code here
-    return
-
-
+    time_unit = find_mult(bits)
+    sentence = bits.replace('0' * time_unit * 7, '   ').replace('0' * time_unit * 3, ' ')
+    morse_code = sentence.replace('1' * time_unit * 3,'-').replace('1' * time_unit, '.').replace('0' * time_unit, '')
+    return morse_code 
+    # words = bits.split('0' * time_unit * 7)
+    # result = []
+    # for word in words:
+    #     letters = word.split('0' * time_unit * 3)
+    #     dots_dashes = []
+    #     for letter in letters:
+    #         dots_dashes.append(letter.split('0' * time_unit))
+    #         result.append(dots_dashes)
+    #         return result
+    
 def decode_morse(morse):
-    # your code here
-    return
-
-
+    letter = ''
+    final_string = ''
+    for index, items in enumerate(morse + ' '):
+        if items != ' ':
+            letter += items
+        elif letter:
+            final_string += MORSE_2_ASCII[letter]
+            letter = ''
+            if index < len(morse) - 2:
+                if morse[index: (index + 3)] == '   ':
+                    final_string += " "
+    return final_string
+    
 if __name__ == '__main__':
     hey_jude_morse = ".... . -.--   .--- ..- -.. ."
     hey_jude_bits = "11001100110011000000110000001111110011001111110011111100000000000000000000011001111110011111100111111000000110011001111110000001111110011001100000011" # noqa
